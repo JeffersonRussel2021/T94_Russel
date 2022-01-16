8fa8676714f6fd31d3862dbf4137f8aeb265c175
JeffersonRussel2021 <89285768+JeffersonRussel2021@users.noreply.github.com>
2022-01-16 12:58:43 +0800

    Add files via upload

---
+++ Ma4FinalTaskPart1JeffersonRussel.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(){
+    
+    for (int a = 1; a < 10; a++)
+    {
+        //printf("%d\n", a);
+        for(int b = a; b < 10; b++)
+        {
+            printf("%d", b);
+        }
+    printf("\n");
+    }
+
+    return 0;
+}
